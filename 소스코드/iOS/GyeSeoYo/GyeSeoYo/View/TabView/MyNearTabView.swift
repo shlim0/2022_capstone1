64e8164176b98eb69dcb39fb4784035ff9aa74d2
imSeongHyeon <jfknfj920@naver.com>
2022-12-28 15:35:21 +0900

    Google MAP API만 추가 (아직 기능 추가 못함)

--- 소스코드/iOS/GyeSeoYo/GyeSeoYo/View/TabView/MyNearTabView.swift
+++ 소스코드/iOS/GyeSeoYo/GyeSeoYo/View/TabView/MyNearTabView.swift
@@ -10,8 +10,7 @@ import SwiftUI
 struct MyNearTabView: View{
     var body: some View {
         VStack{
-//            Button(action: {Network().getData(input_url: <#T##String#>)}, label: {Text("get")})
-//            Button(action: {Network().postData(input_url: <#T##String#>)}, label: {Text("post")})
+
         }
     }
 }
